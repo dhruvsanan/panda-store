4997b8e0b267422fdea573b4a75aa899e19116f7
dhruvsanan <21f1004102@student.onlinedegree.iitm.ac.in>
2024-02-04 19:28:34 +0530

    email update

--- dist/get-payload.js
+++ dist/get-payload.js
@@ -90,8 +90,8 @@ var getPayloadClient = function (_a) {
                     if (!cached.promise) {
                         cached.promise = payload_1.default.init(__assign({ email: {
                                 transport: transporter,
-                                fromAddress: 'hello@joshtriedcoding.com',
-                                fromName: 'DigitalHippo',
+                                fromAddress: 'onboarding@resend.dev',
+                                fromName: 'Dhruv Sanan',
                             }, secret: process.env.PAYLOAD_SECRET, local: (initOptions === null || initOptions === void 0 ? void 0 : initOptions.express) ? false : true }, (initOptions || {})));
                     }
                     _d.label = 1;
