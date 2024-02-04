183782c6f3db96a18671191f530145478a92904d
dhruvsanan <21f1004102@student.onlinedegree.iitm.ac.in>
2024-02-05 04:33:12 +0530

    updates

--- next.config.js
+++ next.config.js
@@ -3,14 +3,15 @@ const nextConfig = {
 	images: {
 		remotePatterns: [
 			{
-				hostname: "localhost",
-				pathname: "**",
-				port : "3000",
 				protocol: "http",
-				
+				hostname: "localhost",
+			},
+			{
+				protocol: "https",
+				hostname: "digitalhippo-production.up.railway.app",
 			},
 		],
 	},
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
