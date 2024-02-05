5a800272a0937003fa59703d54c1b663b1dabfe3
dhruvsanan <21f1004102@student.onlinedegree.iitm.ac.in>
2024-02-06 03:41:50 +0530

    updates

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -13,7 +13,7 @@ export function formatPrice(
     notation?: Intl.NumberFormatOptions['notation']
   } = {}
 ) {
-  const { currency = 'INR', notation = 'compact' } = options
+  const { currency = 'USD', notation = 'compact' } = options
 
   const numericPrice =
     typeof price === 'string' ? parseFloat(price) : price
